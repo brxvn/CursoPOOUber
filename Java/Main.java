73e66ecadd61058f9c5d0fe9570c709d47c2d49c
Bryan Gonzalez <bryangonzalezm@outlook.com>
2021-05-04 20:48:29 -0500

    Contructor method declared in all used languages

--- Java/Main.java
+++ Java/Main.java
@@ -1,5 +1,9 @@
 class Main {
     public static void main(String[] args) {
         System.out.println("hola mundo");
+
+        Car car = new Car("AMK123", new Account("José Cuevas", "JO123"));
+
+        car.printDataCar();
     }
 }
--- Python/main.py
+++ Python/main.py
@@ -1,2 +1,10 @@
+from car import Car
+from account import Account
+
 if __name__ == '__main__':
     print("hola mundo")
+    
+    car = Car("AMS123", Account("Andres Mercado", "AND123"))
+
+    print(vars(car))
+    print(vars(car.driver))
