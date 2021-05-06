23e55862ef01b89f16e5ce5a65435931c2a43954
Bryan Gonzalez <bryangonzalezm@outlook.com>
2021-05-05 19:53:27 -0500

    finished

--- Java/Main.java
+++ Java/Main.java
@@ -2,8 +2,13 @@ class Main {
     public static void main(String[] args) {
         System.out.println("hola mundo");
 
-        Car car = new Car("AMK123", new Account("José Cuevas", "JO123"));
-
+        UberX car = new UberX("AMK123", new Account("José Cuevas", "JO123"), "Tesla", "Model X");
+        car.setPassenger(4);
         car.printDataCar();
+
+        UberVan car2 = new UberVan("123ASD", new Account("Hola Carebola", "JO123"));
+        car2.setPassenger(6);
+        car2.printDataCar();
+
     }
 }
--- Java/UberVan.java
+++ Java/UberVan.java
@@ -5,9 +5,26 @@ class UberVan extends Car {
     Map<String, Map<String, Integer>> typeCarAccepted;
     ArrayList<String> seatsMaterial;
 
+    public UberVan(String license, Account driver){
+        super(license, driver);
+    }
+
     public UberVan(String license, Account driver, Map<String, Map<String, Integer>> typeCarAccepted, ArrayList<String> seatsMaterial ){
         super(license, driver);
         this.typeCarAccepted = typeCarAccepted;
         this.seatsMaterial = seatsMaterial;
+    }
+
+    @Override
+    public void setPassenger(Integer passenger) {
+        if (passenger == 6){
+            this.passenger=passenger;
+        }
+        else System.out.println("Necesitas 6 pasajeros");
+    }
+
+    @Override
+    void printDataCar() {
+        super.printDataCar();
     }
 }
